185aba2846315dc3821ab8aa26ebc0d411ee2ef4
Fabio Girones <fdgirones@gmail.com>
2024-04-25 14:51:02 +0200

    Finished the second section of the course: fundamentals

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,17 @@
 "use strict";
-console.log("hello world");
-let age = 20;
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+numbers.forEach(element => {
+    element.toString();
+});
+let user = [1, 'Mosh'];
+;
+let mySize = 2;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,50 @@
-console.log("hello world");
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
+let level; //any type variable
 
-let age: number = 20;
-if (age < 50) {
-    age += 10
+function render(document: any) {
+    console.log(document);
 }
+
+let numbers: number[] = [1,2,3];
+numbers.forEach(element => {
+    element.toString(); //because we know the type we are using
+});
+
+let user: [number, string] = [1, 'Mosh'];
+
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+//PascalCase
+const enum Size { Small = 1, Medium, Large };
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
+function calculateTax(income: number, taxYear = 2022): number {
+    if (taxYear < 2022) {
+        return income * 1.2;
+    } else {
+        return income * 1.4;
+    }
+}
+
+calculateTax(10_000, 2022);
+
+let employee: {
+    readonly id: number,
+    name?: string
+    retire: (date: Date) => void
+} = {
+    id: 1,
+    retire: (date: Date) => {
+        console.log(date);
+    }
+};
+
+employee name = "Mosh";
+
+
+
